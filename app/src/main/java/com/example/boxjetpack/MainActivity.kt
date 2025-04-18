479b73791041043c760d4d78e99a7f259dfd2a62
HP <timothyatorpaypal@gmail.com>
2025-04-18 13:01:36 +0300

    Designed the box and added some text

--- app/src/main/java/com/example/boxjetpack/MainActivity.kt
+++ app/src/main/java/com/example/boxjetpack/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.NotificationCompat.Style
 import com.example.boxjetpack.ui.theme.BoxJetPackTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,27 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BoxJetPackTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize()){
+                    MyFirstBox()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BoxJetPackTheme {
-        Greeting("Android")
+fun MyFirstBox() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Text(
+            text = "Heyy Dzaddy!!",
+            style = MaterialTheme.typography.headlineLarge
+        )
     }
 }
+
